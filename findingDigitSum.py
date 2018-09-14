ffd58ad16d0fabddf64245e4c4877a9522f6a7d8
Rohun Nanda <rohun.nanda@10525.local>
2018-09-14 11:54:38 -0400

    updated parts

--- findingDigitSum.py
+++ findingDigitSum.py
@@ -6,5 +6,21 @@ def findSum(n):
 		s = s+x
 		n = n//10
 
-	print (s)
+	return(s)
 findSum(720)
+
+def checkHarshad(n):
+
+	if(n%findSum(n)==0):
+		return True
+
+	return False 
+
+print(checkHarshad(720));
+
+low = 80
+high = 100
+
+for i in range(low,high,1):
+
+	print(checkHarshad(i));
